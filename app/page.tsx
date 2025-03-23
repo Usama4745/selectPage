58fa04218c57edbe30e45e219c5717eac2010953
Usama Ahmed <usama4745@gmail.com>
2025-03-23 16:28:49 +0500

    added animation effects and changes text colors and added tag

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 export default function Home() {
 
 
-  const [skips, setSkips] = useState([]);
+  const [skips, setSkips] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
@@ -32,6 +32,10 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="text-white text-3xl">
+        Choose Your Skip Size
+      </div>
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {skips.length === 0 ? (
           <p>No skips available in this area.</p>
@@ -39,11 +43,79 @@ export default function Home() {
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-28 lg:gap-y-16">
 
             {skips.map((skip) => (
-              <div className="relative group  h-48 flex   flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-                <a href="#"
-                  className="block">
-                  <div></div>
-                </a>
+              <div className="group relative rounded-lg  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 
+                  border-[#2A2A2A] hover:border-[#0037C1]/50 
+                  bg-[#1C1C1C] text-white cursor-pointer"
+              >
+                <div className="relative">
+                  <Image
+                    src="https://images.unsplash.com/photo-1590496793929-36417d3117de?q=80&w=800"
+                    alt="4 Yard Skip"
+                    width={800}
+                    height={192}
+                    className="w-full h-36 md:h-48 object-cover rounded-md mb-4"
+                  />
+                  {/* <div className="absolute top-3 right-2 z-20 bg-amber-500 text-white px-3 py-1 
+                      rounded-full text-sm font-medium shadow-md"
+                  >
+                    {skip.size} Yards
+                  </div> */}
+                  {skip.allows_heavy_waste &&
+                    (<div className="absolute bottom-3 right-2 z-20 bg-amber-800 text-white px-3 py-1 
+                      rounded-full text-sm font-medium shadow-md">
+                      Heavy Waste Ok
+                    </div>)}
+                  {skip.allowed_on_road && (
+                    <div className="absolute bottom-3 right-2 z-20 bg-cyan-700 text-white px-3 py-1 
+                      rounded-full text-sm font-medium shadow-md">
+                      Allowed On Road
+                    </div>
+                  )}
+                  <div className="absolute bottom-3 left-2 z-20 space-y-2" />
+                </div>
+
+                <div className="p-4 md:p-6 ">
+                  <h3 className="text-lg md:text-xl font-bold mb-2 text-white">
+                    {skip.size} Yard Skip
+                  </h3>
+
+                  <p className="text-sm text-gray-300 mb-4 md:mb-6">
+                    {skip.hire_period_days} day hire period
+                  </p>
+
+                  <div className="flex justify-between items-center mb-4">
+                    <div>
+                      <span className="text-xl md:text-2xl font-bold text-cyan-600">
+                        £{skip.price_before_vat}
+                      </span>
+                      <span className="text-sm text-gray-300 ml-2">
+                        / week + VAT
+                      </span>
+                    </div>
+                  </div>
+
+                  <button className="w-full py-2.5 md:py-3 px-4 rounded-md transition-all 
+                    flex items-center justify-center space-x-2
+                    bg-[#2A2A2A] text-white hover:bg-[#3A3A3A] hover:border-[#0037C1]"
+                  >
+                    <span>Select This Skip</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-arrow-right w-4 h-4"
+                    >
+                      <path d="M5 12h14" />
+                      <path d="m12 5 7 7-7 7" />
+                    </svg>
+                  </button>
+                </div>
               </div>
             ))}
           </div>
@@ -51,21 +123,6 @@ export default function Home() {
 
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
 
 
       </footer>
